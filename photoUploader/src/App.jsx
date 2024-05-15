91fa212eb23196fc4d79f3715186ee02cdcd2e07
Mohit Sati <mohitt@gmail.com>
2024-05-16 01:52:01 +0530

    new feature

--- photoUploader/src/App.jsx
+++ photoUploader/src/App.jsx
@@ -6,8 +6,17 @@ function App() {
   const inputImage = useRef(null)
   const [Image, imageUploader] = useState("");
 
-  
+  const downloadButton = (ImageSrc , ImgName)=>{
+    const link = document.createElement('a')
+    link.href = ImageSrc
+    link.download = ImgName
+    document.body.appendChild(link)
+    link.click()
+    document.body.removeChild(link)
+    console.log("ho gya download")
+  }
 
+  
   const makeRef = () => {
     inputImage.current.click();
   };
@@ -16,6 +25,8 @@ function App() {
     let file = event.target.files[0]
     console.log(file)
     imageUploader(event.target.files[0])
+    console.log(Image)
+    // some changes were placed
   }
 
   return (
@@ -25,7 +36,11 @@ function App() {
         {Image?<img className='photo' src={URL.createObjectURL(Image)}/> : <img src={PhotoUploader}/>}
         <input type='file' ref={inputImage} onChange={UploadTheImage} style={{ display: 'none' }}></input>
         <p className='aboutPara'>Click the image to upload the photo</p>
+       
       </div>
+       <button className='downloadButton' onClick={()=>{
+          downloadButton(URL.createObjectURL(Image) , "profileImage")
+        }}>Download Image </button>
     </>
   );
 }
